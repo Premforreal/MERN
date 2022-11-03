df3104a7b14d8dd1cf560955f96fb4079f8a5fbf
Premforreal <93695810+Premforreal@users.noreply.github.com>
2022-11-03 14:13:06 +0530

    added asyncHandler before we connect to database

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,33 @@
+const asyncHandler = require('express-async-handler');
 //@desc   gets all goals but after auth it gets specific user goals
 //@route  GET/api/goals
 //@access Private
-const getGoals = (req,res)=>{
+const getGoals = asyncHandler( async (req,res)=>{
     res.status(200).json({msg:'GET Goals'});
-};
+});
 
 //@desc   gets all goals but aft6er auth it gets specific user goals
 //@route  POST/api/goals
 //@access Private
-const setGoal = (req,res)=>{
+const setGoal = asyncHandler(async (req,res)=>{
     if(!req.body.text){
         res.status(400)
         throw new Error("Please add a text field");//this gives a HTML error message
     }
     res.status(200).json({msg:'SET Goals'});
-};
+});
 //@desc   gets all goals but aft6er auth it gets specific user goals
 //@route  PUT/api/goals
 //@access Private
-const updateGoal = (req,res)=>{
+const updateGoal = asyncHandler( async (req,res)=>{
     res.status(200).json({msg:`UPDATE goal ${req.params.id}`});
-};
+});
 //@desc   gets all goals but aft6er auth it gets specific user goals
 //@route  DELETE/api/goals
 //@access Private
-const deleteGoal = (req,res)=>{
+const deleteGoal = asyncHandler(async (req,res)=>{
     res.status(200).json({msg:`DELETE goal ${req.params.id}`});
-};
+});
 
 module.exports={
     getGoals,
