ec70a7095a1a05eab0841910a2f3ff68fe84c622
Premforreal <93695810+Premforreal@users.noreply.github.com>
2022-11-03 11:54:13 +0530

    replaced express def err handler -> middleware📁

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv  = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 const port    = process.env.PORT || 5000
 //https://stackoverflow.com/questions/59428844/listen-eacces-permission-denied-in-windows
 //PORT=5000; gives error. Don't use semicolon.
@@ -11,5 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : false}))
 
 app.use('/api/goals',require('./routes/goalRoutes'));
+
+app.use(errorHandler); //overrides express default error handler
 
 app.listen(port,()=>console.log(`listening on port ${port}`));  
