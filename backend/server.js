a624711b84502f80b796317b625359060fd0429c
Premforreal <93695810+Premforreal@users.noreply.github.com>
2022-11-03 10:45:36 +0530

    basic routes set up done 📡

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const express = require('express');
+const dotenv  = require('dotenv').config();
+const port    = process.env.PORT || 5000
+//https://stackoverflow.com/questions/59428844/listen-eacces-permission-denied-in-windows
+//PORT=5000; gives error. Don't use semicolon.
+
+const app =express();
+
+app.use('/api/goals',require('./routes/goalRoutes'));
+
+app.listen(port,()=>console.log(`listening on port ${port}`));  
