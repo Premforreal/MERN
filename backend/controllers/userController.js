a25f1f9d2a4a2585d2ce000526348a5c765a5769
Premforreal <93695810+Premforreal@users.noreply.github.com>
2022-11-08 09:58:52 +0530

    fixed a small bug 🪲

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -83,7 +83,7 @@ const getMe = asyncHandler( async (req,res)=>{
     const {_id,name,email} = await User.findById(req.user.id);
 
     res.status(200).json({
-        id,
+        id:_id,
         name,
         email,
     });
